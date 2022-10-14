c0a3be8fda34e173b82e4c74d7a3991c858f5eff
Anuj Amin <anuj@royal-05.cs.wisc.edu>
2022-10-13 21:06:01 -0500

    initialized stuff for systems calls

--- xv6-public/sysfile.c
+++ xv6-public/sysfile.c
@@ -442,3 +442,14 @@ sys_pipe(void)
   fd[1] = fd1;
   return 0;
 }
+int sys_settickets(void){
+  /*sets the number of tickets of the calling process. By default, each process should get one ticket; calling this routine makes it such that a process can raise the number of tickets it receives, and thus receive a higher proportion of CPU cycles. This routine should return 0 if successful, and -1 otherwise (if, for example, the caller passes in a number less than one)*/
+  return -1;
+}
+
+int sys_getpinfo(void){
+
+/*returns some information about all running processes, including how many times each has been chosen to run and the process ID of each. You can use this system call to build a variant of the command line program ps, which can then be called to see what is going on. The structure pstat is defined below; note, you cannot change this structure, and must use it exactly as is. This routine should return 0 if successful, and -1 otherwise*/
+
+  return -1;
+}
